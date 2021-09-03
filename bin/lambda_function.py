536f98aad331f30df87aa339d590a8ab03c7ac47
banalna <judas.priest999@gmail.com>
2021-09-03 15:12:46 +0300

    update lambda_function.py

--- bin/lambda_function.py
+++ bin/lambda_function.py
@@ -13,9 +13,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 handler = EntitiesLambdaFunction().get_handler()
 
 
-def lambda_handler(event, context):
+def lambda_handler(event, context) -> dict:
     if isinstance(event, (str, bytes, bytearray)):
         event = json.loads(event)
 
     result = handler(event)
-    return JsonConverter.to_json(result)
+    json_string_result = JsonConverter.to_json(result)
+    
+    if json_string_result:
+        return json.loads(json_string_result)
